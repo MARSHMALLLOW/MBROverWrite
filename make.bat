28db74cff74c8924561a9c0c0cd3566f136f4e78
Anas Fakhfakh <Anas.batal1@gmail.com>
2023-09-10 23:56:57 +0200

    Delete make.bat

--- make.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-"C:\Users\anasb\AppData\Local\bin\NASM\nasm" -f bin MBRSource.asm -o binary.bin
-REM with bin extension.
-"C:\Users\anasb\AppData\Local\bin\NASM\nasm" -f bin MBRSource.asm -o binary.iso
-REM with iso extension.
-"C:\Users\anasb\AppData\Local\bin\NASM\nasm" -f bin MBRSource.asm -o binary.img
-REM with img extension.
-echo successful!
-pause
